d3c34cfc6ad12e67c80c813ac573069b1f0d96c1
DanishLuqman <danish@rosarylabs.ai>
2026-08-12 01:42:56 +0800

    feat: mha with rope test

--- tests/adapters.py
+++ tests/adapters.py
@@ -204,7 +204,16 @@ def run_multihead_self_attention_with_rope(
         Float[Tensor, " ... sequence_length d_model"]: Tensor with the output of running your optimized, batched multi-headed attention
         implementation with the given QKV projection weights and input features.
     """
-    raise NotImplementedError
+    rope = attention.RotaryPositionalEmbedding(theta, d_model // num_heads, max_seq_len)
+    mha = attention.CausalMultiHeadSelfAttention(d_model, num_heads, rope=rope)
+    proj_dict = {
+            "q_proj.weight": q_proj_weight,
+            "k_proj.weight": k_proj_weight,
+            "v_proj.weight": v_proj_weight,
+            "output_proj.weight": o_proj_weight
+            }
+    mha.load_state_dict(proj_dict)
+    return mha(in_features, token_positions)
 
 
 def run_rope(
