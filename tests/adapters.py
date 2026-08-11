21847cd569c1a0b975c4ed19e4b40506a8b6c92e
DanishLuqman <danish@rosarylabs.ai>
2026-08-12 01:40:06 +0800

    feat: wire up mha test

--- tests/adapters.py
+++ tests/adapters.py
@@ -156,7 +156,15 @@ def run_multihead_self_attention(
         Float[Tensor, " ... sequence_length d_model"]: Tensor with the output of running your optimized, batched multi-headed attention
         implementation with the given QKV projection weights and input features.
     """
-    raise NotImplementedError
+    mha = attention.CausalMultiHeadSelfAttention(d_model, num_heads)
+    proj_dict = {
+        "q_proj.weight": q_proj_weight,
+        "k_proj.weight": k_proj_weight,
+        "v_proj.weight": v_proj_weight,
+        "output_proj.weight": o_proj_weight
+        }
+    mha.load_state_dict(proj_dict)
+    return mha(in_features)
 
 
 def run_multihead_self_attention_with_rope(
